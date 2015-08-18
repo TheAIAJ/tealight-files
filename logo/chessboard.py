62ed4bc73e58425b2d6ab0aff51f11311441e90c
TheAIAJ <theaiaj.general@gmail.com>
2015-08-18 12:07:07 +0100

    Running logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -4,11 +4,17 @@ def chessboard(size):
   for i in range(0,8):
     vertical(size)
   
-def vertical(size):
+def verticalUp(size):
   move(size * 8)
   turn(90)
   move(size)
   turn(90)
+
+def verticalDown(size):
+  move(size*8)
+  turn(-90)
+  move(size)
+  turn(-90)
 
 size = 10
 chessboard(size)
