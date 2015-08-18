3532547fdd1198df9f002f2c4dbdda8506de706f
TheAIAJ <theaiaj.general@gmail.com>
2015-08-18 14:49:52 +0100

    Running robot/chess.py

--- robot/chess.py
+++ robot/chess.py
@@ -98,15 +98,3 @@ turn(1)
 for i in range(0,32):
   move()
 turn(-1)
-for i in range(0,31):
-  move()
-turn(-1)
-for i in range(0,4):
-  move()
-turn(-1)
-for i in range(0,30):
-  move()
-turn(1)
-for i in range(0,4):
-  move()
-turn(1)
