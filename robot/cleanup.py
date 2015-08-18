cb46d19ee1fa5269a7420986f0c27e7c0389f1f9
TheAIAJ <theaiaj.general@gmail.com>
2015-08-18 15:23:06 +0100

    Running robot/cleanup.py

--- robot/cleanup.py
+++ robot/cleanup.py
@@ -11,22 +11,7 @@ while i < 500:
   if look() == 'fruit':
     move()
   
-  if touch() == 'fruit':
-    move()
-  
-  if right_side() == 'fruit':
-    move()
-  
-  if left_side() == 'fruit':
-    move()
-  
-  if left_side() != 'fruit' and touch() != 'fruit':
-    turn(-1)
-    
-  if look() == 'fruit':
-    move()
-  
-  if right_side() != 'fruit' and touch() != 'fruit':
+  elif right_side() == 'fruit':
     turn(1)
   
   i+= 1
