02045ee864040b86cc1c9d5984f9c4e6f87169db
TheAIAJ <theaiaj.general@gmail.com>
2015-08-20 10:58:50 +0100

    Closing art/ConnectFour1.py

--- art/ConnectFour1.py
+++ art/ConnectFour1.py
@@ -5,26 +5,6 @@
 
 from tealight.art import (line, spot, circle, box, rectangle, image, text, background, color, screen_width, screen_height)
 
-counterRadius = 50
-
-numCounter = 8
-
-width = screen_width
-
-height = screen_height
-
-box(200, 150, counterRadius * numCounter, counterRadius * numCounter) 
-
-boardList = [[], [], [], [], [], [], [], []]
-
 columnList = [0, 0, 0, 0, 0, 0, 0, 0]
 
-columnList[0] = 1
-
-print columnList[0]
-
-for i in range(0, 8):
-  None
-  
-def handle_mousedown(x, y):
-  None
+def handle_mouse
