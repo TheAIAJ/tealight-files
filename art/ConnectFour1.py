4a19b842506073675000c953d7f7c38b0d0f5e21
TheAIAJ <theaiaj.general@gmail.com>
2015-08-20 12:02:14 +0100

    Running art/ConnectFour1.py

--- art/ConnectFour1.py
+++ art/ConnectFour1.py
@@ -1,10 +1,60 @@
-#TheAIAJ
-#Andrew Jeffery
+from tealight.art import (color, line, spot, circle, box, image, text, background)
 
-#box(x, y, width, height)
+color("blue")
+box(50,950,800,-800)
 
-from tealight.art import (line, spot, circle, box, rectangle, image, text, background, color, screen_width, screen_height)
+xArray = [100, 200, 300, 400, 500, 600, 700, 800]
 
-columnList = [0, 0, 0, 0, 0, 0, 0, 0]
+yArray = [900, 800, 700, 600, 500, 400, 300, 200]
 
-def handle_mouse
+columnCounter = [0, 0, 0, 0, 0, 0, 0, 0]
+
+cCounter1 = 0
+
+x1 = 100
+y1 = 900
+
+x2 = 150
+y2 = 950
+
+x3 = 50
+y3 = 850
+
+turn = 0
+
+for j in range (0,8):
+  for i in range (0,8):
+   color("white")
+   spot(x1, y1, 40)
+   x1 += 100
+  x1 = 100
+  y1 -= 100
+
+
+for b in range (0,7):
+ color("black")
+ line(x2, y2, x2,y2-800)
+ x2 += 100
+  
+for a in range (0,7):
+ color("black")
+ line(x3, y3, x3+800,y3)
+ y3 += -100
+
+def handle_mouseup(x, y):
+  global turn
+  if (x > 50) and (x <=150):
+    if turn == 1:
+      color("red")
+    else:
+      color("yellow")
+    #spot(xArray[0], yArray[0], 40)
+    DrawSpot(0)
+    turn = (turn + 1) % 2
+    print turn
+    
+  
+def DrawSpot(i):
+  global columnCounter
+  spot(xArray[i], yArray[i], 40)
+  columnCounter[i] += 1
