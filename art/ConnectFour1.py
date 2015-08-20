c4ebec864e5f408c85cae9004b2ca3f0a006e7da
TheAIAJ <theaiaj.general@gmail.com>
2015-08-20 10:27:34 +0100

    Running art/ConnectFour1.py

--- art/ConnectFour1.py
+++ art/ConnectFour1.py
@@ -8,4 +8,4 @@ from tealight.art import (line, spot, circle, box, rectangle, image, text,  back
 radius = 10
 
 numCounter = 8
-box(0, 0, radius * numCounter, radius * numCounter) 
+box(100, 100, radius * numCounter, radius * numCounter) 
