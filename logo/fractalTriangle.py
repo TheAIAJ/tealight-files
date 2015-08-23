5cc71c820f438a17c40a30716aa0087d2886c756
TheAIAJ <theaiaj.general@gmail.com>
2015-08-23 11:30:44 +0100

    Running logo/fractalTriangle.py

--- logo/fractalTriangle.py
+++ logo/fractalTriangle.py
@@ -4,14 +4,10 @@ from tealight.logo import move, turn
 
 def segment(scale, detail):
   
-  if detail == 0:
-    #move(scale)
-    None
-  else:
-    for i in range(0, 3):
-      move(scale)
-      turn(120)
-      segment(scale / 2, detail - 1)
+  for i in range(0, 3):
+    move(scale)
+    turn(120)
+    segment(scale / 2, detail - 1)
 
 turn(90)
 segment(200,10)
