0867bc6594787b65b5e6e405e25195dd597eab85
TheAIAJ <theaiaj.general@gmail.com>
2015-08-23 12:54:42 +0100

    Running logo/FractalTree.py

--- logo/FractalTree.py
+++ logo/FractalTree.py
@@ -3,19 +3,18 @@ from tealight.logo import move, turn
 # Draws the tree fractal
 
 
-def segment(scale, detail):
+def segment(scale, detail, angle):
   
   if detail == 0:
     None
   
   else:
     move(scale)
-    
     turn(45)
-    segment(scale / 0.5, detail - 1)
+    segment(scale / angle, detail - 1)
     turn(-90)
-    segment(scale / 0.5, detail - 1)
+    segment(scale / angle, detail - 1)
     turn(45)
     move(-scale)
 move(-200)    
-segment(200,10)
+segment(200,10, 90)
