62213376562a0dd9a42cb051a10f9ef9bcf27802
TheAIAJ <theaiaj.general@gmail.com>
2015-08-23 14:32:56 +0100

    Running robot/trail.py

--- robot/trail.py
+++ robot/trail.py
@@ -19,6 +19,7 @@ while i < 400:
     turn(-1)
   
   elif touch() != 'fruit' and left_side() != 'fruit' and right_side() != 'fruit':
+    turn(-1)
     move()
     for i in range(0, 4):
       turn(-1)
