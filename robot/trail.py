b0e341a75d3fef22ba03a0ea234aafdae7b05d72
TheAIAJ <theaiaj.general@gmail.com>
2015-08-23 14:36:39 +0100

    Running robot/trail.py

--- robot/trail.py
+++ robot/trail.py
@@ -8,23 +8,23 @@ from tealight.robot import (move,
 
 # Add your code here
 i = 0
-turn = 1
+t = 1
 while i < 400:
   if touch() == 'fruit':
     move()
   
   elif right_side() == 'fruit':
     turn(1)
-    turn += 1
+    t += 1
   elif left_side() == 'fruit':
     turn(-1)
-    turn -= 1
+    t -= 1
   elif touch() != 'fruit' and left_side() != 'fruit' and right_side() != 'fruit':
     move()
     for i in range(0, 4):
       turn(1)
-      turn += 1
-      if turn == 3:
+      t += 1
+      if t == 3:
         move()
       if touch() == 'fruit':
         break
