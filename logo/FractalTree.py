56fe2f833754b7e4abe5cbff0f4f39a2b8918dc5
TheAIAJ <theaiaj.general@gmail.com>
2015-08-23 12:29:52 +0100

    Running logo/FractalTree.py

--- logo/FractalTree.py
+++ logo/FractalTree.py
@@ -16,9 +16,9 @@ def segment(scale, detail):
     move(-scale / 2)
     turn(90)
     move(scale / 2)
-    move(-scale / 2
+    move(-scale / 2)
     
-    #segment(scale / 2, detail - 1)
+    segment(scale / 2, detail - 1)
     
 
     move(-scale)
