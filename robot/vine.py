080324504172c5674d763a48dab9f9237b696b08
TheAIAJ <theaiaj.general@gmail.com>
2015-08-23 14:49:07 +0100

    Running robot/vine.py

--- robot/vine.py
+++ robot/vine.py
@@ -15,5 +15,13 @@ while i < 1111:
   elif left_side() == 'fruit':
     turn(-1)
   
+  elif right_side() == 'fruit':
+    turn(1)
+  
+  elif touch() != 'fruit':
+    for i in range(0, 4):
+      turn(1)
+      if touch() == 'fruit':
+        break
   
   i += 1
